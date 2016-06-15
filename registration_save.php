d6651802ddf8b7a037794375adea409f0ff33f6b
Emmanuel Aceves Martínez <raptoream@gmail.com>
2016-06-15 17:34:03 -0500

    Add files via upload

---
+++ registration_save.php
@@ -0,0 +1,41 @@
+	<?php
+echo $firstName=$_POST["firstName"];
+$lastName=$_POST["lastName"];
+$birthdate=$_POST["birthdate"];
+$gender=$_POST["gender"];
+$email=$_POST["email"];
+$address=$_POST["address"];
+$cityState=$_POST["cityState"];
+$zip=$_POST["zip"];
+$country=$_POST["country"];
+$additionalInfo=$_POST["additionalInfo"];
+$username=$_POST["username"];
+$password=$_POST["password"];
+
+$username = "admin";
+$password = "3ZN8AY53";
+
+$m = new MongoClient("mongodb://$username:$password@127.0.0.1:27017",
+  	array("db" => "admin"));
+
+//$m = new MongoClient();
+$coleccion = $m->selectCollection('thisability', 'user');
+
+
+$data = array(	"firstName" => $firstName,
+				"lastName" => $lastName,
+				"birthdate" => $birthdate,
+				"gender" => $gender,
+				"email" => $email,
+				"address" => $address,
+				"cityState" => $cityState,
+				"zip" => $zip,
+				"country" => $country,
+				"additionalInfo" => $additionalInfo,
+				"username" => $username,
+				"password" => $password
+				 
+);
+$coleccion->insert($data);
+
+?>
